210d4445a9d712f7ec5b7e06116d3da9af32144e
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-03-01 08:39:50 -0300

    Spacewar: Include Aospa Dolby Atmos
    * Using my fork: https://github.com/kleidione/hardware_dolby
    
    [1]: Add dolby volume listener effect
    [2]: Bring all stream effects
    [3]: VINTF: Move all vintf files to devicetree
    
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,12 +95,19 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 # Fingerprint
 TARGET_USES_FOD_ZPOS := true
 
-# HIDL
+# VINTF - Compatibility Matrix
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/dolby_framework_matrix.xml \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+
+# VINTF - Manifests
+DEVICE_MANIFEST_FILE += \
+    $(DEVICE_PATH)/configs/vintf/manifest.xml \
+    $(DEVICE_PATH)/configs/vintf/vendor.dolby.hardware.dms@2.0-service.xml \
+    $(DEVICE_PATH)/configs/vintf/vendor.dolby.media.c2.xml
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
