aa45ab35cdd3cf15e92ac2a3355df1e0e648b595
kleidione Freitas <kleidione@gmail.com>
2026-03-24 22:43:57 -0300

    Spacewar: Update from Spacewar_V3.2-260416-1140
    Signed-off-by: kleidione <kleidione@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,7 +163,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2026-03-01
+VENDOR_SECURITY_PATCH := 2026-04-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
