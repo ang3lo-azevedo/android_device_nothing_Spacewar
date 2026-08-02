9534d0584b510107edb13515ddcb550515d23147
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-08-02 23:09:21 +0100

    Remove LineageOS-specific soong namespaces not present in VoltageOS
    - Remove hardware/lineage/interfaces/power-libperfmgr (doesn't exist)
    - Remove hardware/qcom-caf/common/libqti-perfd-client (doesn't exist)
    
    Signed-off-by: Ângelo Azevedo <ang3lo-azevedo@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -37,8 +37,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/nothing \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/qcom-caf/common/libqti-perfd-client \
-    hardware/lineage/interfaces/power-libperfmgr \
     vendor/qcom/opensource/usb/etc
 
 # A/B
