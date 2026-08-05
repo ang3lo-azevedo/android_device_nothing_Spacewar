9856863941cd4c521688cfda8bcd20aa9b5e8b99
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-08-05 18:38:33 +0100

    BoardConfig: bump VENDOR_SECURITY_PATCH to 2026-07-01 (match platform)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,7 +174,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2026-04-16
+VENDOR_SECURITY_PATCH := 2026-07-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
