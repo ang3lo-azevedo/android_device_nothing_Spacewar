3f6a3081abcc394548193b9030388c33b3ec62c2
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-08-06 21:28:52 +0100

    Fix taskbar: use post_boot.sh instead of init service (root context works)

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -6266,3 +6266,7 @@ esac
 misc_link=$(ls -l /dev/block/bootdevice/by-name/misc)
 real_path=${misc_link##*>}
 setprop persist.vendor.mmi.misc_dev_path $real_path
+
+# Fix taskbar by restarting launcher after boot
+(sleep 10 && am force-stop com.android.launcher3) &
+
