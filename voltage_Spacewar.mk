57c63d40f1f7f4b0b11e0fde1b3ca1a0f00fe941
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-08-06 03:36:21 +0100

    Use git clone instead of curl for KernelSU (ServerHive blocks curl)

--- voltage_Spacewar.mk
+++ voltage_Spacewar.mk
@@ -18,7 +18,7 @@ $(shell sed -i 's/ro.config.ringtone=/ro.config.ringtone?=/; s/ro.config.alarm_a
 # Fetch KernelSU source if missing (required by KSU-SUSFS kernel)
 $(shell if [ ! -f kernel/nothing/sm7325/KernelSU/kernel/Kconfig ]; then \
     rm -rf kernel/nothing/sm7325/KernelSU && \
-    cd kernel/nothing/sm7325 && curl -LSs "https://raw.githubusercontent.com/William24hmar/KernelSU/master/kernel/setup.sh" | bash -s master-susfs 2>/dev/null; \
+    git clone -b master-susfs --depth 1 https://github.com/William24hmar/KernelSU.git kernel/nothing/sm7325/KernelSU 2>/dev/null; \
 fi; true)
 
 # Inherit some common Voltage stuff.
