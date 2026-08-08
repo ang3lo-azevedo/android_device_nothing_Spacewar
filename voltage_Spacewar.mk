004c3bb18821eaffb1ef0bc3ec28f11988293a02
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-08-08 22:21:26 +0100

    Fix makefile syntax: redirect fix_ksu_dupes.sh output to /dev/null

--- voltage_Spacewar.mk
+++ voltage_Spacewar.mk
@@ -20,7 +20,7 @@ $(shell if [ ! -f kernel/nothing/sm7325/KernelSU/kernel/Kconfig ]; then \
     rm -rf kernel/nothing/sm7325/KernelSU && \
     git clone -b master-susfs --depth 1 https://github.com/William24hmar/KernelSU.git kernel/nothing/sm7325/KernelSU 2>/dev/null; \
 fi; \
-sh device/nothing/Spacewar/rootdir/bin/fix_ksu_dupes.sh; \
+sh device/nothing/Spacewar/rootdir/bin/fix_ksu_dupes.sh > /dev/null 2>&1; \
 true)
 
 # Inherit some common Voltage stuff.
