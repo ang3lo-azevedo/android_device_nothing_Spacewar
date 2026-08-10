450f08d2c96abf9afac61b5cf2f8fc8d7ea0ee38
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-08-10 16:29:48 +0100

    Fix VoltageOS inherit (was using LineageOS common)

--- voltage_Spacewar.mk
+++ voltage_Spacewar.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from Spacewar device
 $(call inherit-product, device/nothing/Spacewar/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := voltage_Spacewar
